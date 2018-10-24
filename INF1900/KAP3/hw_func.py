e47897270b5a3f2347ec66fbc39c584a4bfbdaff
Jon-Magnus Rosenblad <jonmagnusr@gmail.com>
2018-10-24 09:53:28 +0200

    Added exercises for week 3

---
+++ INF1900/KAP3/hw_func.py
@@ -0,0 +1,15 @@
+def hw1():
+	return 'Hello, World!'
+
+def hw2():
+	print hw1()
+
+
+def hw3(a,b):
+	return a + b
+
+print hw1()
+hw2()
+print hw3('Hello, ', 'World!')
+print hw3('Python, ', 'function')
+
